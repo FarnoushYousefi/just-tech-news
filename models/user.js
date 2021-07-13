ca8b420877699ed231b1a90122023f838ffb0707
Farnoush <farnoush.yousefikejani@uwaterloo.ca>
2021-07-13 14:51:30 -0400

    add the hooker

--- models/user.js
+++ models/user.js
@@ -1,6 +1,6 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
-
+const bcrypt = require('bcrypt');
 // create our User model
 class User extends Model {}
 
@@ -11,34 +11,49 @@ User.init(
       type: DataTypes.INTEGER,
       allowNull: false,
       primaryKey: true,
-      autoIncrement: true
+      autoIncrement: true,
     },
     username: {
       type: DataTypes.STRING,
-      allowNull: false
+      allowNull: false,
     },
     email: {
       type: DataTypes.STRING,
       allowNull: false,
       unique: true,
       validate: {
-        isEmail: true
-      }
+        isEmail: true,
+      },
     },
     password: {
       type: DataTypes.STRING,
       allowNull: false,
       validate: {
-        len: [4]
-      }
-    }
+        len: [4],
+      },
+    },
   },
   {
+    // hooks: {
+    //   // set up beforeCreate lifecycle "hook" functionality
+    //   beforeCreate(userData) {
+    //     return bcrypt.hash(userData.password, 10).then((newUserData) => {
+    //       return newUserData;
+    //     });
+    //   },
+    // }
+    hooks: {
+      // set up beforeCreate lifecycle "hook" functionality
+      async beforeCreate(newUserData) {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      },
+    },
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'user'
+    modelName: 'user',
   }
 );
 
