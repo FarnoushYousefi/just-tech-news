479982d5a4727683d4c8fed9854b2454f8746c2f
Farnoush <farnoush.yousefikejani@uwaterloo.ca>
2021-07-16 12:15:00 -0400

    error

--- models/index.js
+++ models/index.js
@@ -1,6 +1,7 @@
 const User = require('./User');
 const Post = require('./Post');
 const Vote = require('./Vote');
+const Comment = require('./Comment');
 // create associations
 User.belongsToMany(Post, {
   through: Vote,
@@ -27,4 +28,16 @@ User.hasMany(Vote, {
 Post.hasMany(Vote, {
   foreignKey: 'post_id',
 });
-module.exports = { User, Post, Vote };
+Comment.belongsTo(User, {
+  foreignKey: 'user_id',
+});
+Comment.belongsTo(Post, {
+  foreignKey: 'post_id',
+});
+User.hasMany(Comment, {
+  foreignKey: 'user_id',
+});
+Post.hasMany(Comment, {
+  foreignKey: 'post_id',
+});
+module.exports = { User, Post, Vote, Comment };
