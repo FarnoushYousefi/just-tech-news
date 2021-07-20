4c9966ac425beeda4ffe54198eaef88e1b6abce7
Farnoush <farnoush.yousefikejani@uwaterloo.ca>
2021-07-19 23:07:27 -0400

    feature/homepage-view

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const routes = require('./routes');
+const path = require('path');
 const sequelize = require('./config/connection');
 
 const app = express();
@@ -10,6 +11,7 @@ app.use(express.urlencoded({ extended: true }));
 
 // turn on routes
 app.use(routes);
+// app.use(express.static(path.join(__dirname, 'public')));
 
 // turn on connection to db and server
 sequelize.sync({ force: true }).then(() => {
